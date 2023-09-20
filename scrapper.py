e70ea0fac609ba4662a17b5da5087e9f04c7d633
kshah09 <kshah409@gatech.edu>
2023-09-20 23:37:19 +0530

    Extracted Resume Text from scrapped

--- scrapper.py
+++ scrapper.py
@@ -9,7 +9,9 @@ keywords = ['Financial Analyst','Investment Banking','Asset Management','Taxatio
             'Front-End Developer','IT Project Manager','Software Architect','Blockchain','Web Developer','Network Engineer','Cloud Computing','Machine Learning','Back-End Developer',
             'UX/UI Designer','QA','IoT','Cybersecurity Analyst','Database Administrator','DevOps Engineer','Big Data','Full-Stack Developer',
             'Mobile App Developer','IT Support Specialist','Network Security']
-total_pages_per_category = 2
+total_pages_per_category = 50
+
+
 keywords = list(set(keywords))
 refined_keywords =[x.replace("/","%2F").replace(" ","+").lower() for x in keywords]
 url_dict = {z:[f"https://www.postjobfree.com/resumes?q={x}&l=&radius=25&p={y}" for y in range(total_pages_per_category)] for x,z in zip(refined_keywords,keywords)}
@@ -27,5 +29,25 @@ for keywrd in keywords:
     tmp['resume_link']=resume_links
     tmp['keyword'] = keywrd
     data = pd.concat([data,tmp])
-data.to_excel('ResumeLinks.xlsx',index=False)
     
+data.to_excel('ResumeLinks.xlsx',index=False)
+
+#Fetch Resume Text Info
+data = pd.read_excel('ResumeLinks.xlsx')
+tqdm.tqdm.pandas()
+def extract_info(link):
+    try:
+        c = requests.get(link).content
+        soup = BeautifulSoup(c, 'html5lib') 
+        main_se  = soup.find('div',{"class":"normalText"})
+        return ("\n".join([x.text for x in main_se.find_all('p')])  , soup.find('h1').text , soup.find('a',{'class':'colorLocation'}).text)
+    except:  
+        return (None,None,None)
+
+data['resume_text']=None
+data['title']=None
+data['location']=None
+data[['resume_text','title','location']] = data.resume_link.progress_apply(extract_info)
+data.to_excel('ExtractedInfo.xlsx',index=False)
+
+
